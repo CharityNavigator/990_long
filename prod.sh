c69c7335a6097c23a0ad3fed1c4ee0cb01f0810d
David Borenstein <dborenstein@charitynavigator.org>
2017-10-25 20:59:32 +0000

    Made parse_xml run speculative

--- prod.sh
+++ prod.sh
@@ -1,4 +1,4 @@
 spark-submit ./python/load_paths.py --prod
 spark-submit --conf spark.speculation=true ./python/load_xml.py
-spark-submit ./python/parse_xml.py
+spark-submit --conf spark.speculation=true ./python/parse_xml.py
 spark-submit ./python/merge_xml.py
