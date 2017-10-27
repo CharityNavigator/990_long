ad2d5f43ee3a317a034e01712e3d21851cad9a9d
David Bruce Borenstein <dborenstein@charitynavigator.org>
2017-10-27 00:48:55 +0000

    Fixed an egregious partitioning bug in merge_xml

--- python/partition_xml.py
+++ python/partition_xml.py
@@ -21,7 +21,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--input", action="store", help="Path to Parquet file containing xpath-value pairs.", default = "990_long/parsed")
 parser.add_argument("--output", action="store", help="Path in which to store result. Can be local or S3.", default = "s3a://cn-validatathon")
 parser.add_argument("--timestamp", action="store_true", help="If true, append the timestamp to the output path.")
-parser.add_argument("--partitions", type=int, action="store", help="Number of partitions to use for the join.", default=400)
 args = parser.parse_args()
 
 if args.timestamp:
@@ -32,22 +31,9 @@ else:
 
 outputPath = args.output + suffix
 
-cc = spark.read.csv("concordance.csv", header=True) \
-        .select(col("xpath"),
-                col("variable_name").alias("variable"),
-                col("data_type").alias("var_type"),
-                col("form"),
-                col("part"),
-                col("scope"),
-                col("location_code").alias("location"))
-
-standardize = udf(lambda xpath : "/Return/" + xpath.strip(), StringType())
-df = spark.read.parquet(args.input) \
-        .repartition(args.partitions) \
-        .withColumn("xpath", standardize(col("xpath"))) \
-
-df.join(cc, "xpath", "left") \
-        .write \
+spark.read.parquet(args.input) \
+        .repartition("form", "part") \
+        .write.partitionBy("form", "part") \
         .parquet(outputPath)
 
 print "*** Process complete."
